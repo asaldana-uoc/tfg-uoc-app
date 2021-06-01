ff177c20385445fd09c38d2cc222533a59fab602
Angel Saldaña Lopez <asaldana@uoc.edu>
2021-06-01 13:27:36 +0200

    Actualitzar comentaris

--- tf/ci/main.tf
+++ tf/ci/main.tf
@@ -1,6 +1,6 @@
-# Es crea un trigger específic en Google Cloud Build per a que
-# s'executi cada vegada que es detecti un canvi nou en la branca principal main.
-# Només s'aplicarà terraform quan els canvis afectin a l'entorn devel.
+# Es crea un trigger específic en Google Cloud Build per a que s'executi cada vegada que es creï una Pull Request
+# en el repositori, sobre la qual, es passaran els tests unitaris definits en el codi Go. No s'executarà aquest trigger
+# quan es detectin canvis en els directoris k8s i tf ja que no contenen codi de l'aplicació.
 resource "google_cloudbuild_trigger" "ci_tests_rigger" {
   name          = "tfg-uoc-ci-tests-app"
   description   = "Trigger que s'executarà en cada nova Pull Request creada i commits posteriors"
@@ -17,6 +17,9 @@ resource "google_cloudbuild_trigger" "ci_tests_rigger" {
   }
 }
 
+# Aquest un trigger de Google Cloud Build construirà una imatge de Docker cada vegada que hi hagi un commit
+# nou en la branca principal main. No s'executarà aquest procés automatitzat quan els canvis hagin succeït
+# als directoris k8s i tf ja que no contenen codi de l'aplicació.
 resource "google_cloudbuild_trigger" "ci_build_trigger" {
   name          = "tfg-uoc-ci-build-app"
   description   = "Trigger que s'executarà cada nou commit a la branca main per construir la imatge de docker"
